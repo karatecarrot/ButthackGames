47acc8d246004cfa7ea06f9475c1dc8cc811df11
DYLANSPC\Dylan <dylanclarke099@gmail.com>
2018-09-15 20:47:58 +0930

    Added character model (WIP), UFPS addon, modified player controller & added example gun

---
+++ VoxelZombie/Assets/Scripts/GunPickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunPickup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<FireGun>().PickupAR();
+            Destroy(gameObject);
+        }
+    }
+}
